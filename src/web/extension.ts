9306b700128a0410542e1830d946850c9aa7a42e
Tim b <tim@tbradford.com>
2024-11-19 23:04:40 -0600

    initial commit

---
+++ src/web/extension.ts
@@ -0,0 +1,77 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(
+		vscode.window.registerCustomEditorProvider(
+			'kotlin.highlightjs.editor',
+			new KotlinHighlightJsEditorProvider(context),
+			{
+				webviewOptions: { retainContextWhenHidden: true },
+				supportsMultipleEditorsPerDocument: false
+			}
+		)
+	);
+}
+
+class KotlinHighlightJsEditorProvider implements vscode.CustomTextEditorProvider {
+	constructor(private readonly context: vscode.ExtensionContext) {}
+
+	public async resolveCustomTextEditor(
+		document: vscode.TextDocument,
+		webviewPanel: vscode.WebviewPanel,
+		_token: vscode.CancellationToken
+	): Promise<void> {
+		// Set up the initial content for the webview
+		webviewPanel.webview.options = { enableScripts: true };
+
+		const updateWebview = () => {
+			webviewPanel.webview.html = this.getHtmlForWebview(
+				webviewPanel.webview,
+				document.getText()
+			);
+		};
+
+		// Initial content
+		updateWebview();
+
+		// Update the content when the document changes
+		const changeDocumentSubscription = vscode.workspace.onDidChangeTextDocument(
+			e => {
+				if (e.document.uri.toString() === document.uri.toString()) {
+					updateWebview();
+				}
+			}
+		);
+
+		webviewPanel.onDidDispose(() => {
+			changeDocumentSubscription.dispose();
+		});
+	}
+
+	private getHtmlForWebview(webview: vscode.Webview, code: string): string {
+		const escapedCode = code
+			.replace(/&/g, '&amp;')
+			.replace(/</g, '&lt;')
+			.replace(/>/g, '&gt;');
+
+		return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/default.min.css"
+  >
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/highlight.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/languages/kotlin.min.js"></script>
+  <script>
+    hljs.highlightAll();
+  </script>
+</head>
+<body>
+  <pre><code class="language-kotlin">${escapedCode}</code></pre>
+</body>
+</html>`;
+	}
+}
